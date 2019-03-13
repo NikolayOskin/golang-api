ad81abc42ac0437029d18ed1959008d0b35a8053
Oskin <wskeal@gmail.com>
2019-03-14 01:07:19 +0300

    updating db

--- db/db.go
+++ db/db.go
@@ -5,10 +5,17 @@ import (
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func Connect() *gorm.DB {
-    db, err := gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/golang-api?charset=utf8&parseTime=True&loc=Local")
+var DB *gorm.DB
+
+func Open() (*gorm.DB, error) {
+    var err error
+    DB, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/golang-api?charset=utf8&parseTime=True&loc=Local")
     if err != nil {
-        panic(err.Error())
+        return nil, err
     }
-    return db
+    return DB, nil
+}
+ 
+func Close() error {
+    return DB.Close()
 }
