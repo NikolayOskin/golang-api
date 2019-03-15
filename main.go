a641810e6ec38f195620be35f896f7376fc3c846
Oskin <wskeal@gmail.com>
2019-03-15 11:45:35 +0300

    updating main.go (adding routes and db logic)

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "api/controllers/postController"
+	"api/controllers/postController"
+	"api/db"
+	"api/models"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/posts", postController.GetAll).Methods("GET")
-    router.HandleFunc("/post/{id}", postController.GetPost).Methods("GET")
-    router.HandleFunc("/posts/create", postController.Create).Methods("POST")
-    router.HandleFunc("/posts/{id}", postController.Delete).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8080", router))
+	if _, err := db.Open(); err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	db.DB.DropTableIfExists("posts")
+	db.DB.AutoMigrate(&models.Post{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/posts", postController.GetAll).Methods("GET")
+	router.HandleFunc("/posts/{id}", postController.GetPost).Methods("GET")
+	router.HandleFunc("/posts/create", postController.Create).Methods("POST")
+	router.HandleFunc("/posts/{id}", postController.Update).Methods("PUT")
+	router.HandleFunc("/posts/{id}", postController.Delete).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
