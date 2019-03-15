b9d01416768210d8e63c2b7f1a9f2283a8d35af0
Oskin <wskeal@gmail.com>
2019-03-15 12:10:35 +0300

    update db connection

--- db/db.go
+++ db/db.go
@@ -1,21 +1,21 @@
 package db
 
 import (
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var DB *gorm.DB
 
 func Open() (*gorm.DB, error) {
-    var err error
-    DB, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/golang-api?charset=utf8&parseTime=True&loc=Local")
-    if err != nil {
-        return nil, err
-    }
-    return DB, nil
+	var err error
+	DB, err = gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/golang-api?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		return nil, err
+	}
+	return DB, nil
 }
- 
+
 func Close() error {
-    return DB.Close()
+	return DB.Close()
 }
