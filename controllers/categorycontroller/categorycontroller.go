c5d7ccb77beb283ac05bc67e148478fa79f23668
Oskin <wskeal@gmail.com>
2019-03-23 02:59:08 +0300

    updating category controller & repository

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,13 +1,29 @@
 package categorycontroller
 
-import "net/http"
+import (
+	"api/controllers/handler"
+	"api/models"
+	"api/repository/categoryrepository"
+	"net/http"
 
-// Show - get posts by category
+	"github.com/gorilla/mux"
+)
+
+// Show - get category with related posts
 func Show(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	category := categoryrepository.GetByIDWithPosts(params["id"])
+	handler.RespondJSON(w, http.StatusOK, category)
 }
 
 // Store - store category in db
 func Store(w http.ResponseWriter, r *http.Request) {
-
+	var category models.Category
+	category = models.Category{Slug: r.FormValue("slug"), Title: r.FormValue("title")}
+	result, err := categoryrepository.Store(category)
+	if err != nil {
+		handler.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	handler.RespondJSON(w, http.StatusOK, result)
 }
--- repository/categoryrepository/categoryrepository.go
+++ repository/categoryrepository/categoryrepository.go
@@ -1,1 +1,21 @@
 package categoryrepository
+
+import (
+	"api/db"
+	"api/models"
+)
+
+// GetByIDWithPosts - get category with related posts
+func GetByIDWithPosts(id string) models.Category {
+	var category models.Category
+	db.DB.Where("id = ?", id).Preload("Posts").First(&category)
+	return category
+}
+
+// Store - store category in db
+func Store(category models.Category) (models.Category, error) {
+	if err := db.DB.Create(&category).Error; err != nil {
+		return category, err
+	}
+	return category, nil
+}
