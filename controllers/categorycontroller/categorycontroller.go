61400cca2743d5e58736b6b573366b9a64b97c9d
Oskin <wskeal@gmail.com>
2019-03-23 01:02:39 +0300

    adding category repository & controller

---
+++ controllers/categorycontroller/categorycontroller.go
@@ -0,0 +1,13 @@
+package categorycontroller
+
+import "net/http"
+
+// Show - get posts by category
+func Show(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Store - store category in db
+func Store(w http.ResponseWriter, r *http.Request) {
+
+}
