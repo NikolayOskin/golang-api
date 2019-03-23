68bb9ceb521f56e1aa781c7b19d3054d9a5fbf17
Oskin <wskeal@gmail.com>
2019-03-24 01:36:40 +0300

    updating repositories

--- repository/categoryrepository/categoryrepository.go
+++ repository/categoryrepository/categoryrepository.go
@@ -5,17 +5,40 @@ import (
 	"api/models"
 )
 
+var category models.Category
+
+// GetByID - get category
+func GetByID(id string) models.Category {
+	db.DB.Where("id = ?", id).First(&category)
+	return category
+}
+
 // GetByIDWithPosts - get category with related posts
 func GetByIDWithPosts(id string) models.Category {
-	var category models.Category
 	db.DB.Where("id = ?", id).Preload("Posts").First(&category)
 	return category
 }
 
 // Store - store category in db
-func Store(category models.Category) (models.Category, error) {
+func Store(category *models.Category) (*models.Category, error) {
 	if err := db.DB.Create(&category).Error; err != nil {
 		return category, err
 	}
 	return category, nil
+}
+
+// UpdateByID - update existed category
+func UpdateByID(id string, newCategory *models.Category) (models.Category, error) {
+	db.DB.Where("id = ?", id).First(&category)
+	category.Slug = newCategory.Slug
+	category.Title = newCategory.Title
+	if err := db.DB.Save(&category).Error; err != nil {
+		return category, err
+	}
+	return category, nil
+}
+
+// DeleteByID - delete existed category
+func DeleteByID() {
+
 }
--- repository/postsRepository/postsRepository.go
+++ repository/postsRepository/postsRepository.go
@@ -1,43 +1,40 @@
 package postsRepository
 
 import (
-    "api/db"
-    "api/models"
+	"api/db"
+	"api/models"
 )
 
+var post models.Post
+
 func GetAll() []models.Post {
-    db := db.Connect()
-    defer db.Close()
-    var posts []models.Post
-    db.Find(&posts)
-    return posts
+	var posts []models.Post
+	db.DB.Find(&posts)
+	return posts
 }
 
 func GetById(id string) models.Post {
-    db := db.Connect()
-    defer db.Close()
-    var post models.Post
-    db.Where("id = ?", id).First(&post)
-    return post
+	db.DB.Where("id = ?", id).First(&post)
+	return post
+}
+
+func GetByIdWithCategory(id string) models.Post {
+	db.DB.Where("id = ?", id).Preload("Category").First(&post)
+	return post
 }
 
 func Create(post models.Post) (models.Post, error) {
-    db := db.Connect()
-    defer db.Close()
-    if err := db.Create(&post).Error; err != nil {
-        return post, err
-    }
-    return post, nil
+	if err := db.DB.Create(&post).Error; err != nil {
+		return post, err
+	}
+	return post, nil
 }
 
 func Delete(id string) bool {
-    db := db.Connect()
-    defer db.Close()
-    var post models.Post
-    db.Where("id = ?", id).First(&post)
-    if post.ID != 0 {
-        db.Unscoped().Delete(&post)
-        return true
-    }    
-    return false
+	db.DB.Where("id = ?", id).First(&post)
+	if post.ID != 0 {
+		db.DB.Unscoped().Delete(&post)
+		return true
+	}
+	return false
 }
